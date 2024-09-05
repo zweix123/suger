fb26c3de8e52019afa583252ecca98b6ddd4263e
zweix123 <1979803044@qq.com>
2024-09-05 19:57:29 +0800

    [feat] add assert

---
+++ suger.go
@@ -0,0 +1,17 @@
+package suger
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func Assert(condition bool, message string) {
+	if !condition {
+		_, file, line, ok := runtime.Caller(1)
+		if !ok {
+			file = "???"
+			line = 0
+		}
+		panic(fmt.Sprintf("%s:%d: %s", file, line, message))
+	}
+}
