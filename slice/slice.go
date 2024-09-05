ab36ec4c7e1cbd81629af47fefbae31ddbef58e9
zweix123 <1979803044@qq.com>
2024-09-05 22:56:19 +0800

    [feat] add slice function and unit test

--- slice/slice.go
+++ slice/slice.go
@@ -11,3 +11,25 @@ func Contain[T comparable](l []T, e T) bool {
 	}
 	return false
 }
+
+func Equal[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	n := len(a)
+	for i := 0; i < n; i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func Reverse[T any](src []T) (dst []T) {
+	dst = make([]T, len(src))
+	for i, j := 0, len(src)-1; i < len(src); i, j = i+1, j-1 {
+		dst[i] = src[j]
+		dst[j] = src[i]
+	}
+	return
+}
---
+++ slice/slice_test.go
@@ -0,0 +1,75 @@
+package slice
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestContain(t *testing.T) {
+	type args struct {
+		l []int
+		e int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"empty", args{[]int{}, 1}, false},
+		{"not found", args{[]int{1, 2, 3}, 4}, false},
+		{"found", args{[]int{1, 2, 3}, 2}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Contain(tt.args.l, tt.args.e); got != tt.want {
+				t.Errorf("Contain() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEqual(t *testing.T) {
+	type args struct {
+		a []int
+		b []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"empty", args{[]int{}, []int{}}, true},
+		{"not equal", args{[]int{1, 2, 3}, []int{1, 2, 4}}, false},
+		{"equal", args{[]int{1, 2, 3}, []int{1, 2, 3}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Equal(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Equal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestReverse(t *testing.T) {
+	type args struct {
+		src []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"empty", args{[]int{}}, []int{}},
+		{"one", args{[]int{1}}, []int{1}},
+		{"two", args{[]int{1, 2}}, []int{2, 1}},
+		{"three", args{[]int{1, 2, 3}}, []int{3, 2, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Reverse(tt.args.src); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Reverse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
