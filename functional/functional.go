80fe73be16481196efa63540277ad2eeb93d81eb
zhengweixin <zhengweixin@didiglobal.com>
2024-09-10 15:59:44 +0800

    [refactor] take map is parallet default

--- functional/functional.go
+++ functional/functional.go
@@ -1,12 +1,26 @@
 package functional
 
+import "sync"
+
 // Map manipulates a slice and transforms it to a slice of another type.
+// `iteratee` is call in parallel. Result keep the same order.
 func Map[T any, R any](collection []T, iteratee func(item T) R) []R {
 	result := make([]R, len(collection))
 
-	for i := range collection {
-		result[i] = iteratee(collection[i])
+	var wg sync.WaitGroup
+	wg.Add(len(collection))
+
+	for i, item := range collection {
+		go func(_item T, _i int) {
+			res := iteratee(_item)
+
+			result[_i] = res
+
+			wg.Done()
+		}(item, i)
 	}
+
+	wg.Wait()
 
 	return result
 }
