446852f9e621c78f6ef1c4b7000d0a52abccd095
zweix123 <1979803044@qq.com>
2024-10-15 21:41:31 +0800

    Supplement necessary functional programming functions and perform single tests

--- functional/functional.go
+++ functional/functional.go
@@ -1,26 +1,57 @@
 package functional
 
-import "sync"
+import (
+	"sync"
 
+	"github.com/zweix123/suger/common"
+)
+
+// copy from https://github.com/samber/lo/blob/master/slice.go
+// Map manipulates a slice and transforms it to a slice of another type.
+func Map[T any, R any](collection []T, iteratee func(item T, index int) R) []R {
+	result := make([]R, len(collection))
+
+	for i := range collection {
+		result[i] = iteratee(collection[i], i)
+	}
+
+	return result
+}
+
+// copy from https://github.com/samber/lo/blob/master/parallel/slice.go
 // Map manipulates a slice and transforms it to a slice of another type.
 // `iteratee` is call in parallel. Result keep the same order.
-func Map[T any, R any](collection []T, iteratee func(item T) R) []R {
+func MapParallel[T any, R any](collection []T, iteratee func(item T, index int) R) []R {
 	result := make([]R, len(collection))
 
 	var wg sync.WaitGroup
 	wg.Add(len(collection))
 
 	for i, item := range collection {
-		go func(_item T, _i int) {
-			res := iteratee(_item)
+		go func(_item T, _i int) { // nolint
+			defer wg.Done()
+			defer common.HandlePanic(func(_ string, _ int, _ any) {})
+			res := iteratee(_item, _i)
 
 			result[_i] = res
-
-			wg.Done()
 		}(item, i)
 	}
 
 	wg.Wait()
+
+	return result
+}
+
+// copy from https://github.com/samber/lo/blob/master/slice.go
+// Filter iterates over elements of collection, returning an array of all elements predicate returns truthy for.
+func Filter[T any, Slice ~[]T](collection Slice, predicate func(item T, index int) bool) Slice {
+	result := make(Slice, 0, len(collection))
+
+	for i := range collection {
+		if predicate(collection[i], i) {
+			result = append(result, collection[i])
+		}
+	}
 
 	return result
 }
--- functional/functional_test.go
+++ functional/functional_test.go
@@ -1,29 +1,79 @@
 package functional
 
 import (
-	"reflect"
+	"strconv"
 	"testing"
 )
 
 func TestMap(t *testing.T) {
-	type args struct {
-		collection []int
-		iteratee   func(item int) int
+	result1 := Map([]int{1, 2, 3, 4}, func(x int, _ int) string {
+		return "Hello"
+	})
+	if len(result1) != 4 {
+		t.Errorf("expected length of result1 to be 4, got %d", len(result1))
 	}
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"test1", args{[]int{1, 2, 3, 4, 5}, func(item int) int {
-			return item * 2
-		}}, []int{2, 4, 6, 8, 10}},
+	if result1[0] != "Hello" || result1[1] != "Hello" || result1[2] != "Hello" || result1[3] != "Hello" {
+		t.Errorf("expected result1 to be [Hello, Hello, Hello, Hello], got %v", result1)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Map(tt.args.collection, tt.args.iteratee); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Map() = %v, want %v", got, tt.want)
-			}
-		})
+	result2 := Map([]int64{1, 2, 3, 4}, func(x int64, _ int) string {
+		return strconv.FormatInt(x, 10)
+	})
+	if len(result2) != 4 {
+		t.Errorf("expected length of result2 to be 4, got %d", len(result2))
+	}
+	if result2[0] != "1" || result2[1] != "2" || result2[2] != "3" || result2[3] != "4" {
+		t.Errorf("expected result2 to be [1, 2, 3, 4], got %v", result2)
+	}
+}
+
+func TestMapParallel(t *testing.T) {
+	result1 := MapParallel([]int{1, 2, 3, 4}, func(x int, _ int) string {
+		return "Hello"
+	})
+	if len(result1) != 4 {
+		t.Errorf("expected length of result1 to be 4, got %d", len(result1))
+	}
+	if result1[0] != "Hello" || result1[1] != "Hello" || result1[2] != "Hello" || result1[3] != "Hello" {
+		t.Errorf("expected result1 to be [Hello, Hello, Hello, Hello], got %v", result1)
+	}
+	result2 := MapParallel([]int64{1, 2, 3, 4}, func(x int64, _ int) string {
+		return strconv.FormatInt(x, 10)
+	})
+	if len(result2) != 4 {
+		t.Errorf("expected length of result2 to be 4, got %d", len(result2))
+	}
+	if result2[0] != "1" || result2[1] != "2" || result2[2] != "3" || result2[3] != "4" {
+		t.Errorf("expected result2 to be [1, 2, 3, 4], got %v", result2)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	r1 := Filter([]int{1, 2, 3, 4}, func(x int, _ int) bool {
+		return x%2 == 0
+	})
+	if len(r1) != 2 {
+		t.Errorf("expected length of r1 to be 2, got %d", len(r1))
+	}
+	if r1[0] != 2 || r1[1] != 4 {
+		t.Errorf("expected r1 to be [2, 4], got %v", r1)
+	}
+
+	r2 := Filter([]string{"", "foo", "", "bar", ""}, func(x string, _ int) bool {
+		return len(x) > 0
+	})
+	if len(r2) != 2 {
+		t.Errorf("expected length of r2 to be 2, got %d", len(r2))
+	}
+	if r2[0] != "foo" || r2[1] != "bar" {
+		t.Errorf("expected r2 to be [foo, bar], got %v", r2)
+	}
+
+	type myStrings []string
+	allStrings := myStrings{"", "foo", "bar"}
+	nonempty := Filter(allStrings, func(x string, _ int) bool {
+		return len(x) > 0
+	})
+	if _, ok := interface{}(nonempty).(myStrings); !ok {
+		t.Errorf("type preserved: expected nonempty to be of type []string, got %T", nonempty)
 	}
 }
