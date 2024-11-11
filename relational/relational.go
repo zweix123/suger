bde122a2d5892d11ec7726d67474a9dfa3fe4fd2
zweix123 <1979803044@qq.com>
2024-11-11 20:12:08 +0800

    [feat] relational algebra: group by

---
+++ relational/relational.go
@@ -0,0 +1,15 @@
+package relational
+
+// copy from https://github.com/samber/lo/blob/master/slice.go
+// GroupBy returns an object composed of keys generated from the results of running each element of collection through iteratee.
+func GroupBy[T any, U comparable, Slice ~[]T](collection Slice, iteratee func(item T) U) map[U]Slice {
+	result := map[U]Slice{}
+
+	for i := range collection {
+		key := iteratee(collection[i])
+
+		result[key] = append(result[key], collection[i])
+	}
+
+	return result
+}
---
+++ relational/relational_test.go
@@ -0,0 +1,36 @@
+package relational
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGroupBy(t *testing.T) {
+	result1 := GroupBy([]int{0, 1, 2, 3, 4, 5}, func(i int) int {
+		return i % 3
+	})
+
+	if len(result1) != 3 {
+		t.Errorf("expected result1 to be 3, got %v", len(result1))
+	}
+	if !reflect.DeepEqual(result1, map[int][]int{
+		0: {0, 3},
+		1: {1, 4},
+		2: {2, 5},
+	}) {
+		t.Errorf("expected result1 to be %v, got %v", map[int][]int{
+			0: {0, 3},
+			1: {1, 4},
+			2: {2, 5},
+		}, result1)
+	}
+
+	type myStrings []string
+	allStrings := myStrings{"", "foo", "bar"}
+	nonempty := GroupBy(allStrings, func(i string) int {
+		return 42
+	})
+	if reflect.TypeOf(nonempty[42]).String() != "relational.myStrings" {
+		t.Errorf("expected type of nonempty[42] to be relational.myStrings, got %v", reflect.TypeOf(nonempty[42]))
+	}
+}
